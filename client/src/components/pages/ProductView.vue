ec588ee3a584d97f50c1b6129959d65ae524b69d
Jennifer Bland <ratracegrad@gmail.com>
2020-10-22 07:29:18 -0400

    Added product review section to product page

--- client/src/components/pages/ProductView.vue
+++ client/src/components/pages/ProductView.vue
@@ -5,7 +5,7 @@
     </div>
     <app-not-found v-else-if="!product" />
     <div v-else-if="product">
-      <h2 class="text-center text-danger mb-5">{{product.model}}</h2>
+      <h2 class="text-center text-danger mb-5">{{ product.model }}</h2>
       <div class="row">
         <div class="col-xl-4 col-md-12 text-center">
           <div class="image">
@@ -13,22 +13,124 @@
           </div>
         </div>
         <div class="col-xl-8 col-md-12">
-          <h3 class="text-dark">Model: {{product.model}}</h3>
+          <h3 class="text-dark">Model: {{ product.model }}</h3>
           <h5 class="text-muted">
             Made By
-            <span class="text-dark">{{product.brand}}</span>
+            <span class="text-dark">{{ product.brand }}</span>
           </h5>
           <h5 class="text-primary">
             <span class="text-muted">Price:</span>
-            ₹ {{formatPrice(product.price)}}
+            ₹ {{ formatPrice(product.price) }}
           </h5>
-          <h6 class="text-danger border-bottom border-dark">Some info about the Product</h6>
-          <p class="text-dark">{{product.description}}</p>
+          <h6 class="text-danger border-bottom border-dark">
+            Some info about the Product
+          </h6>
+          <p class="text-dark">{{ product.description }}</p>
+
+          <div class="card bg-light p-4 mb-3">
+            <h3 class="text-dark">Add a Review:</h3>
+            <form @submit.prevent="processReview()">
+              <div class="form-group">
+                <label for="review">Review:</label>
+                <textarea
+                  name="review"
+                  id="review"
+                  rows="3"
+                  class="form-control"
+                  placeholder="Enter your review"
+                  v-model="review.comment"
+                ></textarea>
+              </div>
+              <div class="form-group">
+                <label for="name">Name:</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="name"
+                  name="name"
+                  placeholder="Enter your name"
+                  v-model="review.name"
+                />
+              </div>
+              <div class="form-group">
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    id="inlineCheckbox1"
+                    value="1"
+                    v-model="review.stars"
+                  />
+                  <label class="form-check-label" for="inlineCheckbox1"
+                    >1 star</label
+                  >
+                </div>
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    id="inlineCheckbox2"
+                    value="2"
+                    v-model="review.stars"
+                  />
+                  <label class="form-check-label" for="inlineCheckbox2"
+                    >2 stars</label
+                  >
+                </div>
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    id="inlineCheckbox3"
+                    value="3"
+                    v-model="review.stars"
+                  />
+                  <label class="form-check-label" for="inlineCheckbox3"
+                    >3 stars</label
+                  >
+                </div>
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    id="inlineCheckbox4"
+                    value="4"
+                    v-model="review.stars"
+                  />
+                  <label class="form-check-label" for="inlineCheckbox4"
+                    >4 stars</label
+                  >
+                </div>
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    id="inlineCheckbox5"
+                    value="5"
+                    v-model="review.stars"
+                  />
+                  <label class="form-check-label" for="inlineCheckbox5"
+                    >5 stars</label
+                  >
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Submit Review
+              </button>
+            </form>
+          </div>
+
           <router-link to="/" class="btn btn-outline-primary btn-sm mr-3">
             <p class="mb-0">Back to Products</p>
           </router-link>
-          <button @click="addToCart(product)" class="btn btn-sm btn-outline-warning">
-            <i v-if="!hasCarted(product._id)" class="fa-2x fal fa-cart-arrow-down"></i>
+          <button
+            @click="addToCart(product)"
+            class="btn btn-sm btn-outline-warning"
+          >
+            <i
+              v-if="!hasCarted(product._id)"
+              class="fa-2x fal fa-cart-arrow-down"
+            ></i>
             <p v-else class="mb-0">In Cart</p>
           </button>
         </div>
@@ -43,7 +145,7 @@ import NotFound from "@/components/pages/NotFound.vue";
 export default {
   name: "App-Product-View",
   components: {
-    "app-not-found": NotFound,
+    "app-not-found": NotFound
   },
   computed: {
     ...mapGetters(["formatPrice", "hasCarted"]),
@@ -52,14 +154,29 @@ export default {
     },
     product() {
       return this.$store.state.viewSingleProduct;
-    },
+    }
+  },
+  data() {
+    return {
+      review: {
+        comment: "",
+        name: "",
+        stars: null
+      }
+    };
   },
   async mounted() {
     await this.$store.dispatch("fetchSingleProduct", this.$route.params.id);
   },
   methods: {
     ...mapActions(["addToCart"]),
-  },
+    async processReview() {
+      await this.$store.dispatch("handleReview", { review: this.review, id: this.product._id });
+      this.review.comment = '';
+      this.review.name = '';
+      this.review.stars = null;
+    }
+  }
 };
 </script>
 
